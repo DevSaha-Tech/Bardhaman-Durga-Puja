20ff7161639f8c5cac3d3ffbbfbddd84221d1c99
ranadebsaha <ranadebsaha2002@gmail.com>
2026-09-06 19:39:15 +0530

    updated

--- frontend/src/components/MapView.jsx
+++ frontend/src/components/MapView.jsx
@@ -106,9 +106,10 @@ export default function MapView({
       className="z-0"
     >
       <TileLayer
-        url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>, Tiles style by <a href="https://www.hotosm.org/">HOT</a>'
-        maxZoom={19}
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        maxNativeZoom={19}
+        maxZoom={22}
       />
       
       {/* Auto Pan Hook */}
